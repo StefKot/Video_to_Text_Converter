ba217fdc0cc501ae78c2853315b1b74e8e4d845c
Stepan Kotov <StefanKotov22@gmail.com>
2023-10-29 20:10:59 +0300

    Added filename changing option, added timebar

--- converter.py
+++ converter.py
@@ -2,6 +2,7 @@ import wave, math, contextlib
 from alive_progress import alive_bar
 import speech_recognition as sr
 from moviepy.editor import AudioFileClip
+import os
 from os import path
 
 file_name = input("Enter the filename you want to convert to text (without file permissions): ")
@@ -9,8 +10,9 @@ file_name = input("Enter the filename you want to convert to text (without file 
 audio_file_name = file_name + ".wav"
 video_file_name = file_name + ".mp4"
 
-audioclip = AudioFileClip(video_file_name)
-audioclip.write_audiofile(audio_file_name)
+if (not os.path.isfile(audio_file_name)):
+    audioclip = AudioFileClip(video_file_name)
+    audioclip.write_audiofile(audio_file_name)
 
 
 """ For mp3 audio to wav audio
@@ -25,15 +27,18 @@ with contextlib.closing(wave.open(audio_file_name,'r')) as f:
     duration = frames / float(rate)
 total_duration = math.ceil(duration / 60)
 r = sr.Recognizer()
-f = open(file_name + ".txt", "w")
 
-
-with alive_bar(total_duration) as bar:
-    for i in range(total_duration):
-        with sr.AudioFile(audio_file_name) as source:       
-            audio = r.record(source, offset=i*20, duration=20)
-            f.write(r.recognize_google(audio, language="ru-RU"))
-            f.write(" ")
-            bar()
+with open(file_name + ".txt", "w") as f:
+    with alive_bar(total_duration) as bar:
+        for i in range(total_duration):
+            with sr.AudioFile(audio_file_name) as source:      
+                r.adjust_for_ambient_noise(source) 
+                audio = r.record(source, offset=i*60, duration=60)
+                try:
+                    f.write(str(r.recognize_google(audio, language="ru-RU", show_all=False)))
+                    # print(r.recognize_google(audio, language="ru-RU", show_all=False))
+                    f.write(" ")
+                except Exception as e:
+                    print(f"Exception: {e}\nDuration: {i}")
+                bar()
             
-    f.close()
