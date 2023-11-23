4da41a7de643439c78f4da8668d3b5890875d8a4
Stepan Kotov <StefanKotov22@gmail.com>
2023-11-23 21:04:01 +0300

    Config file was added, path were changed

---
+++ config.py
@@ -0,0 +1,3 @@
+mp4_path = "/GitHub/Video_to_Text_Translator/mp4"
+wav_path = "/GitHub/Video_to_Text_Translator/wav"
+txt_path = "/GitHub/Video_to_Text_Translator/txt"
--- converter.py
+++ converter.py
@@ -5,15 +5,32 @@ from moviepy.editor import AudioFileClip
 import os
 from os import path
 
-file_name = input("Enter the filename you want to convert to text (without file permissions): ")
-# For video to wav audio
-audio_file_name = file_name + ".wav"
-video_file_name = file_name + ".mp4"
+import config
 
+# Noise Reduction Control parameter
+NOISE = False 
+
+
+file_name = input("Enter the filename you want to convert to text (without file permissions): ")
+
+# Creating directories for mp4, wav and txt files
+if (not os.path.exists(config.mp4_path)):
+    os.mkdir(config.mp4_path)
+
+if (not os.path.exists(config.wav_path)):
+    os.mkdir(config.wav_path)
+
+if (not os.path.exists(config.txt_path)):
+    os.mkdir(config.txt_path)
+
+# For video to wav audio
+audio_file_name = os.path.join(config.wav_path, file_name + ".wav")
+video_file_name = os.path.join(config.mp4_path, file_name + ".mp4")
+
+# Checking the existence of a wav file, for re-processing into a txt file
 if (not os.path.isfile(audio_file_name)):
     audioclip = AudioFileClip(video_file_name)
     audioclip.write_audiofile(audio_file_name)
-
 
 """ For mp3 audio to wav audio
 sound = AudioSegment.from_mp3("04.mp3")
@@ -21,23 +38,23 @@ sound.export("transcript.wav", format="wav")
 audio_file_name = "transcript.wav"
 """
 
-with contextlib.closing(wave.open(audio_file_name,'r')) as f:
-    frames = f.getnframes()
-    rate = f.getframerate()
+with contextlib.closing(wave.open(audio_file_name, 'r')) as file:
+    frames = file.getnframes()
+    rate = file.getframerate()
     duration = frames / float(rate)
 total_duration = math.ceil(duration / 60)
-r = sr.Recognizer()
+record = sr.Recognizer()
 
-with open(file_name + ".txt", "w") as f:
+with open(os.path.join(config.txt_path, file_name + ".txt"), "w") as file:
     with alive_bar(total_duration) as bar:
         for i in range(total_duration):
-            with sr.AudioFile(audio_file_name) as source:      
-                r.adjust_for_ambient_noise(source) 
-                audio = r.record(source, offset=i*60, duration=60)
+            with sr.AudioFile(audio_file_name) as source: 
+                if NOISE:    
+                    record.adjust_for_ambient_noise(source) 
+                audio = record.record(source, offset=i*60, duration=60)
                 try:
-                    f.write(str(r.recognize_google(audio, language="ru-RU", show_all=False)))
-                    # print(r.recognize_google(audio, language="ru-RU", show_all=False))
-                    f.write(" ")
+                    file.write(str(record.recognize_google(audio, language="ru-RU", show_all=False)))
+                    file.write(" ")
                 except Exception as e:
                     print(f"Exception: {e}\nDuration: {i}")
                 bar()
